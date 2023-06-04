5a5d03a160d88b36914b7b0be931027032ecdf75
pritansh sahsani <82302289+pritansh-sahsani@users.noreply.github.com>
2023-06-05 00:57:11 +0530

    update

--- index.py
+++ index.py
@@ -1,16 +1,23 @@
 import streamlit as st
+import torch
+import librosa
+import sounddevice as sd
+import numpy as np
+import soundfile as sf
+from transformers import Wav2Vec2Tokenizer, Wav2Vec2ForCTC
 import requests
 import json
-import os
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
-# title and favicon ------------------------------------------------------------
+import sys
+import queue
+
+# Basic page setup and definitions------------------------------------------------------------
 
 st.set_page_config(
     page_title="Speech-to-Text Transcription App", layout="wide"
 )
-
-# App layout width -------------------------------------------------
-
 
 def _max_width_():
     max_width_str = f"max-width: 1200px;"
@@ -27,8 +34,6 @@ def _max_width_():
 
 
 _max_width_()
-
-# header -------------------------------------------------
 
 c30, c31, c32 = st.columns([2.5, 1, 3])
 
@@ -48,202 +53,109 @@ with c32:
 
 
 st.text("")
-st.markdown(
-    f"""
-                    The speech to text recognition is done via the [Facebook's Wav2Vec2 model.](https://huggingface.co/facebook/wav2vec2-large-960h)
-                    """
-)
+c1, c2, c3 = st.columns([1, 4, 1])
+with c2:
+    st.markdown(
+            f"""The speech to text recognition is done via the [Facebook's Wav2Vec2 model.](https://huggingface.co/facebook/wav2vec2-large-960h)"""
+    )
 st.text("")
 
-# region Main
-
-# multi navbar -------------------------------------------------
-
-
+# Defining Home page
 def main():
     pages = {
-        "Free mode (2MB per API call)": demo,
-        "Full mode (with your API key)": API_key,
+        "Home": record,
     }
 
     if "page" not in st.session_state:
         st.session_state.update(
             {
                 # Default page
-                "page": "Home",
+                "page": record,
             }
         )
 
-    with st.sidebar:
-        page = st.radio("Select your mode", tuple(pages.keys()))
+    pages["Home"]()
 
-    pages[page]()
+# recording -------------------------------------------------
 
+# define queue 
+q = queue.Queue()
 
-# endregion main
+# callback function
+def callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
 
-# Free mode -------------------------------------------------
-
-
-def demo():
-
+# recording voice from microphone 
+def record():
     c1, c2, c3 = st.columns([1, 4, 1])
     with c2:
+        transcript=""
+        start_button=st.button("Start Recording")
 
-        with st.form(key="my_form"):
+        if start_button:
+            stop_button=st.button("Stop Recording")
+            with sf.SoundFile("temp_input_storage.wav", mode='w', samplerate=16000, channels=1) as file:
+                with sd.InputStream(samplerate=16000, channels=1, callback=callback):
+                    while True:
+                        file.write(q.get())
+                        file_size = sys.getsizeof(file)
+                        if file_size < (2 * 1024 * 1024): # File less than 2MB
+                            transcript=transcripter()
+                            st.write(transcript)
+                        else:
+                            download_transcript=st.download_button(
+                                "Download the transcription",
+                                transcript,
+                                file_name=None,
+                                mime=None,
+                                key=None,
+                                help=None,
+                                on_click=None,
+                                args=None,
+                                kwargs=None,
+                            )
+                            break
+                        
+                        # stop the recording on button click
+                        if stop_button:
+                            download_transcript=st.download_button(
+                                "Download the transcription",
+                                transcript,
+                                file_name=None,
+                                mime=None,
+                                key=None,
+                                help=None,
+                                on_click=None,
+                                args=None,
+                                kwargs=None,
+                            )
+                            break
 
-            f = st.file_uploader("", type=[".wav"])
+def transcripter():
+    # Load your API key from an environment variable or secret management service
+    speech, rate = librosa.load("temp_input_storage.wav", sr=16000)
+    # api_token = "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"
+    # headers = {"Authorization": f"Bearer {api_token}"}
+    # API_URL = "https://api-inference.huggingface.co/models/facebook/wav2vec2-base-960h"
+    # def query(data):
+    #     response = requests.request("POST", API_URL, headers=headers, data=data)
+    #     return json.loads(response.content.decode("utf-8"))
+    
+    # data = query(speech.all())
 
-            submit_button = st.form_submit_button(label="Transcribe")
+    # values_view = data.values()
+    # value_iterator = iter(values_view)
+    # text_value = next(value_iterator)
+    # text_value = text_value.lower()
 
-    if f is not None:
-        path_in = f.name
-        # Get file size from buffer
-        # Source: https://stackoverflow.com/a/19079887
-        old_file_position = f.tell()
-        f.seek(0, os.SEEK_END)
-        getsize = f.tell()  # os.path.getsize(path_in)
-        f.seek(old_file_position, os.SEEK_SET)
-        getsize = round((getsize / 1000000), 1)
-
-        if getsize < 2:  # File more than 2MB
-            # To read file as bytes:
-            bytes_data = f.getvalue()
-
-            # Load your API key from an environment variable or secret management service
-            api_token = st.secrets["API_TOKEN"]
-
-            headers = {"Authorization": f"Bearer {api_token}"}
-            API_URL = "https://api-inference.huggingface.co/models/facebook/wav2vec2-base-960h"
-
-            def query(data):
-                response = requests.request("POST", API_URL, headers=headers, data=data)
-                return json.loads(response.content.decode("utf-8"))
-
-            data = query(bytes_data)
-
-            values_view = data.values()
-            value_iterator = iter(values_view)
-            text_value = next(value_iterator)
-            text_value = text_value.lower()
-
-            st.success(text_value)
-
-            c0, c1 = st.columns([2, 2])
-
-            with c0:
-                st.download_button(
-                    "Download the transcription",
-                    text_value,
-                    file_name=None,
-                    mime=None,
-                    key=None,
-                    help=None,
-                    on_click=None,
-                    args=None,
-                    kwargs=None,
-                )
-
-        else:
-            st.warning(
-                "🚨 The file you uploaded is more than 2MB! Please switch to full mode ↖️ and add your HuggingFace API key."
-            )
-            st.stop()
-
-    else:
-        path_in = None
-        st.stop()
-
-
-# Custom API key mode -------------------------------------------------
-
-
-def API_key():
-
-    c1, c2, c3 = st.columns([1, 4, 1])
-    with c2:
-
-        with st.form(key="my_form"):
-
-            text_input = st.text_input("Enter your HuggingFace API key")
-
-            f = st.file_uploader("", type=[".wav"])
-
-            submit_button = st.form_submit_button(label="Transcribe")
-
-    if not submit_button:
-
-        st.stop()
-
-    else:
-
-        try:
-
-            if f is not None:
-                path_in = f.name
-                # Get file size from buffer
-                # Source: https://stackoverflow.com/a/19079887
-                old_file_position = f.tell()
-                f.seek(0, os.SEEK_END)
-                getsize = f.tell()  # os.path.getsize(path_in)
-                f.seek(old_file_position, os.SEEK_SET)
-                getsize = round((getsize / 1000000), 1)
-                # st.caption("The size of this file is: " + str(getsize) + "MB")
-                # getsize
-
-                if getsize < 30:  # File more than 30MB
-
-                    # To read file as bytes:
-                    bytes_data = f.getvalue()
-
-                    api_token = text_input
-
-                    headers = {"Authorization": f"Bearer {api_token}"}
-                    API_URL = "https://api-inference.huggingface.co/models/facebook/wav2vec2-base-960h"
-
-                    def query(data):
-                        response = requests.request(
-                            "POST", API_URL, headers=headers, data=data
-                        )
-                        return json.loads(response.content.decode("utf-8"))
-
-                    data = query(bytes_data)
-
-                    # data = query(bytes_data)
-
-                    values_view = data.values()
-                    value_iterator = iter(values_view)
-                    text_value = next(value_iterator)
-                    text_value = text_value.lower()
-
-                    st.success(text_value)
-
-                    c0, c1 = st.columns([2, 2])
-
-                    with c0:
-                        st.download_button(
-                            "Download the transcription",
-                            text_value,
-                            file_name=None,
-                            mime=None,
-                            key=None,
-                            help=None,
-                            on_click=None,
-                            args=None,
-                            kwargs=None,
-                        )
-
-                else:
-                    st.error(
-                        "This app is still in early development so we've maxed out the file size limit. Please try again with a smaller file."
-                    )
-                    st.stop()
-
-            else:
-                path_in = None
-
-        except ValueError:
-            "ValueError"
-
+    # return text_value
+    input_values = tokenizer(speech, return_tensors='pt').input_values
+    logits = model(input_values).logits
+    predicted_ids = torch.argmax(logits, dim=-1)
+    transcription = tokenizer.decode(predicted_ids[0])
+    return transcription
+        
 if __name__ == "__main__":
     main()
